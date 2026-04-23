3f340bf17a1683109c96a146d55fb223b45d3096
Harshit Dhiman <harshitdhiman16@gmail.com>
2026-04-23 12:06:54 +0530

    UserDetailsServices added

--- src/main/java/com/example/todo_api/service/UserDetailsServiceImpl.java
+++ src/main/java/com/example/todo_api/service/UserDetailsServiceImpl.java
@@ -1,4 +1,23 @@
 package com.example.todo_api.service;
 
-public class UserDetailsServiceImpl {
+import com.example.todo_api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+        throws UsernameNotFoundException{
+            return userRepo.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found" + username));
+        }
+
+
 }
