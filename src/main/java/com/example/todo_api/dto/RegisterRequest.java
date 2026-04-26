8b2e0a0ba9181e16633248d42057e6a1077bc9e5
Harshit Dhiman <harshitdhiman16@gmail.com>
2026-04-26 17:47:30 +0530

    adding DTO files and services

--- src/main/java/com/example/todo_api/dto/RegisterRequest.java
+++ src/main/java/com/example/todo_api/dto/RegisterRequest.java
@@ -1,4 +1,9 @@
 package com.example.todo_api.dto;
 
+import lombok.Data;
+
+@Data
 public class RegisterRequest {
+    private String username;
+    private String password;
 }
--- src/main/java/com/example/todo_api/service/AuthService.java
+++ src/main/java/com/example/todo_api/service/AuthService.java
@@ -1,4 +1,38 @@
 package com.example.todo_api.service;
 
+import com.example.todo_api.dto.LoginRequest;
+import com.example.todo_api.dto.RegisterRequest;
+import com.example.todo_api.entity.User;
+import com.example.todo_api.repository.UserRepository;
+import com.example.todo_api.security.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
+    private final UserRepository userRepo;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+
+    public String register(RegisterRequest req){
+        if(userRepo.existsByUsername(req.getUsername())){
+            throw new RuntimeException("Username already exists");
+        }
+        User user = new User();
+        user.setUsername(req.getUsername());
+        user.setPassword(passwordEncoder.encode(req.getPassword()));
+        userRepo.save(user);
+        return "User registered successfully";
+    }
+
+    public String login(LoginRequest req){
+        //Throws BadCredentilsException if wrong password
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(req.getUsername(), req.getPassword()));
+        return jwtUtil.generateToken(req.getUsername());
+    }
 }
