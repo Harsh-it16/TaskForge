45409fa797155335e27a8d6287756b283a503cdb
Harshit Dhiman <harshitdhiman16@gmail.com>
2026-04-28 22:18:26 +0530

    Adding auth controller and updating application.properties files

--- src/main/java/com/example/todo_api/controller/AuthController.java
+++ src/main/java/com/example/todo_api/controller/AuthController.java
@@ -1,4 +1,34 @@
 package com.example.todo_api.controller;
 
+
+import com.example.todo_api.dto.LoginRequest;
+import com.example.todo_api.dto.RegisterRequest;
+import com.example.todo_api.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
 public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest req) {
+        return ResponseEntity.ok(authService.register(req));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest req) {
+        return ResponseEntity.ok(authService.login(req));
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<String> me(@AuthenticationPrincipal UserDetails user) {
+        return ResponseEntity.ok("Hello" + user.getUsername() + " | Role: "+ user.getAuthorities());
+    }
 }
